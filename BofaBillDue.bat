b9cc9f4cdc8315dbb6cc2865ed2ea64d8da97f8f
RayNieva <raynieva@gmail.com>
2019-02-10 12:21:45 -0500

    New automation scripts

---
+++ BofaBillDue.bat
@@ -0,0 +1,5 @@
+@echo off
+@echo Bofa Bill is due schedule a payment!
+powershell c:\users\rayni\desktop\sendmailBofaBillDue.ps1
+@echo Sending email alert
+pause ...
---
+++ PayPalBillDue.bat
@@ -0,0 +1,5 @@
+@echo off
+@echo PayPal Bill is due schedule a payment!
+powershell c:\users\rayni\desktop\sendmailPayPalBillDue.ps1
+@echo Sending email alert
+pause ...
