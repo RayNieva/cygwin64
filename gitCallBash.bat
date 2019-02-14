34be02563917f78f799b7d0d1e248cc688867661
RayNieva <raynieva@gmail.com>
2019-02-14 11:05:39 -0500

    added echo at end

---
+++ gitCallBash.bat
@@ -0,0 +1,20 @@
+cd c:\cygwin64
+call gitBashCygwin.bat
+cd c:\users\rayni\desktop
+call c:\users\rayni\desktop\gitBashDesktop.bat
+cd c:\users\rayni\appdata\Roaming
+call c:\users\rayni\appdata\Roaming\gitBashRoaming.bat
+cd c:\users\rayni\appdata\Roaming\org
+call c:\users\rayni\appdata\Roaming\org\gitBashOrg.bat
+cd c:\users\rayni\documents\Projects\Python
+call c:\users\rayni\documents\Projects\Python\gitBashPython.bat
+cd c:\users\rayni\documents\Projects\Ruby
+call c:\users\rayni\documents\Projects\Ruby\gitBashRuby.bat
+cd c:\users\rayni\documents\Projects\VB
+call c:\users\rayni\documents\Projects\VB\gitBashVB.bat
+cd c:\cygwin64
+echo Returning to cygwin64 executable files
+
+
+
+
