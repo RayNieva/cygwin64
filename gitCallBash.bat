313661daf78d75b18953ede46d1b14a2257a8d29
RayNieva <raynieva@gmail.com>
2019-02-16 08:22:09 -0500

    Forgot this one yesterday

--- gitCallBash.bat
+++ gitCallBash.bat
@@ -15,6 +15,7 @@ call c:\users\rayni\documents\Projects\VB\gitBashVB.bat
 cd c:\cygwin64
 echo Returning to cygwin64 executable files
 echo Make sure backup update on Raynieva2 computer using git pull!
+pause .....
 
 
 
